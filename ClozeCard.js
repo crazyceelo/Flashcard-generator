269b2c2204c907683ade9868d28fe4be263b6264
Carlos Perez <solracias@gmail.com>
2017-05-29 09:37:53 -0700

    created initial constructors, exported and required them

---
+++ ClozeCard.js
@@ -0,0 +1,13 @@
+var basicCard = require("./BasicCard.js");
+var inquire = require("inquirer");
+
+
+function Cloze(text, cloze){
+    this.text = text;
+    this.cloze = cloze;
+}
+
+
+
+
+module.exports - ClozeCard;
