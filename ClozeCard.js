6fe3ff2b2599c00acae00915465c1d451ec52ae4
Carlos Perez <solracias@gmail.com>
2017-05-29 09:57:35 -0700

    can now console.log question1 and asnwer1 as a sample to see if it is working

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,13 +1,17 @@
-var basicCard = require("./BasicCard.js");
+var BasicCard = require("./BasicCard.js");
 var inquire = require("inquirer");
 
 
-function Cloze(text, cloze){
+function ClozeCard(text, cloze){
     this.text = text;
     this.cloze = cloze;
 }
 
+var washington = new BasicCard();
+washington.front = "question 1";
+washington.back = "answer 1";
 
+console.log(washington.front);
+console.log(washington.back);
 
-
-module.exports - ClozeCard;
+module.exports = ClozeCard;
